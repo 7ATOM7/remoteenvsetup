77b84e19857837922de6b9f75ca6f95f64a029e8
7ATOM7 <10832380+7ATOM7@users.noreply.github.com>
2022-07-03 15:18:36 +0530

    Terraform script for remote server --complete

---
+++ DigitalOceanSetup/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+provider "digitalocean" {
+    token = var.do_token
+}
+
+data "digitalocean_ssh_key" "do_infra" {
+  name = "do_infra"
+}
+
+resource "digitalocean_droplet" "dev" {
+    image = "ubuntu-20-04-x64"
+    name = "dev"
+    region = "nyc3"
+    size = "s-1vcpu-1gb"
+    ssh_keys = [
+      data.digitalocean_ssh_key.do_infra.id
+    ]
+}
